fd5ceff6dc89799829392f36fd3c6adb0b36cd4b
Namhoon Choe <sysynh2012@gmail.com>
2023-11-09 11:42:49 +0900

    [Feat] Offline-Todolist #3
    TodoCard, deleteTodo 작성함,
    
    EditTodo 작성해야함

--- OptimisticFront/src/Components/TodoCard.tsx
+++ OptimisticFront/src/Components/TodoCard.tsx
@@ -1,6 +1,126 @@
+import { Button, Flex, Text, chakra } from "@chakra-ui/react";
+import React, { useState } from "react";
+import DeleteIcon from "./SvgIcons/DeleteIcon";
+import EditIcon from "./SvgIcons/EditIcon";
+import SaveIcon from "./SvgIcons/SaveIcon";
+import { Options } from "./types";
 
-export default function TodoCard() {
+type TodoCardPropType = {
+  progress?: Options;
+  task: string;
+  id: string;
+  deleteTodo:(id: string) => void
+};
+
+const CardLayout = chakra(Flex, {
+  baseStyle: {
+    width: "100%",
+    height: "4rem",
+    padding: "0.5rem",
+    justifyContent:"space-between",
+    alignItems: "center",
+    boxShadow:` 0 1px rgba(113,	128,	150, 0.4)`,
+  },
+});
+
+const EditFormContainer = chakra(Flex, {
+  display: "flex",
+  justifyContent: "spaceBetween",
+  alignItems: "center",
+});
+
+const EditForm = chakra("form", {
+  baseStyle: {
+    width: "100%",
+    height: "80%",
+    display: "flex",
+    justifyContent: "spaceBetween",
+    alignItems: "center",
+    padding: `0.25rem 0.5rem`,
+  },
+});
+
+const EditFormInput = chakra("input", {
+  baseStyle: {
+    width: "88%",
+    height: "80%",
+    borderRadius: "0.8rem",
+    border: "1px",
+    borderColor: "gray.400",
+    paddingX: "0.5rem",
+    textTransform: "capitalize",
+  },
+});
+
+const IconContainer = chakra(Flex, {
+  baseStyle: {
+    width: "1.5rem",
+    height: "1.5rem",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+const TodoTask = chakra(Text, {
+  baseStyle: {
+    textTransform: "capitalize",
+    fontWeight: "semibold",
+    color: "gray.500",
+  },
+});
+
+const ControlPanel = chakra(Flex, {
+  baseStyle: {},
+});
+
+const TodoCard: React.FC<TodoCardPropType> = ({  task, id, deleteTodo }) => {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  //const [newTask, setNewTask] = useState<string>("");
+
+  const toggleEdit = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleDelte = () => {
+    deleteTodo(id)
+  }
+
   return (
-    <div>TodoCard</div>
-  )
-}
+    <CardLayout>
+      {isEditing ? (
+        <EditFormContainer>
+          <EditForm>
+            <EditFormInput />
+            <Button
+              width={"2rem"}
+              height={"2rem"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              padding={0}
+              type={"submit"}
+            >
+              <SaveIcon />
+            </Button>
+          </EditForm>
+        </EditFormContainer>
+      ) : (
+        <>
+          {/**진행상황 아이콘 표시하기 */}
+          <TodoTask>{task}</TodoTask>
+
+          {/**진행상황 선택할 수 있도록 수정 */}
+          <ControlPanel>
+            <IconContainer onClick={toggleEdit}>
+              <EditIcon />
+            </IconContainer>
+            <IconContainer onClick={handleDelte}>
+              <DeleteIcon />
+            </IconContainer>
+          </ControlPanel>
+        </>
+      )}
+    </CardLayout>
+  );
+};
+
+export default TodoCard;
--- OptimisticFront/src/Components/TodoList.tsx
+++ OptimisticFront/src/Components/TodoList.tsx
@@ -1,59 +1,51 @@
-import {
-  Button,
-  Flex,
-  Text,
-  chakra
-} from "@chakra-ui/react"
-import { useState } from "react"
-import ProgressSelector from "./ProgressSelector"
-import AddIcon from "./SvgIcons/AddIcon"
-import { Options, OptionsType, TodoListItemType } from "./types"
+import { Button, Flex, Text, chakra } from "@chakra-ui/react";
+import { useState } from "react";
+import ProgressSelector from "./ProgressSelector";
+import AddIcon from "./SvgIcons/AddIcon";
+import TodoCard from "./TodoCard";
+import { Options, OptionsType, TodoListItemType } from "./types";
 
-const TodoListLayout = chakra(Flex,{
-  baseStyle:{
-    width:"25rem",
-    maxHeight:"30rem",
-    flexDirection:"column",
-    justifyContent:"start",
-    alignItems:"center",
-    borderRadius:"0.8rem",
-    overflow:"hidden",
-    boxShadow:"xs",
-  }
-})
+const TodoListLayout = chakra(Flex, {
+  baseStyle: {
+    width: "25rem",
+    maxHeight: "30rem",
+    flexDirection: "column",
+    justifyContent: "start",
+    alignItems: "center",
+    borderRadius: "0.8rem",
+    overflow: "hidden",
+    boxShadow: "xs",
+  },
+});
 
 const TodoListHeader = chakra(Flex, {
-  baseStyle:{
-    flexDirection:"column",
-    width:"100%",
-    height:"7rem",
- 
-  }
-})
+  baseStyle: {
+    flexDirection: "column",
+    width: "100%",
+    height: "7rem",
+  },
+});
 
-
-const TodoListHeading = chakra(Flex,{
-  baseStyle:{
-    width:"100%",
-    height:"4rem",
-    justifyContent:"center",
-    alignItems:"center",
-    backgroundColor:"green.400"
-  }
-})
-
-
+const TodoListHeading = chakra(Flex, {
+  baseStyle: {
+    width: "100%",
+    height: "4rem",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "green.400",
+  },
+});
 
 const ListContainer = chakra(Flex, {
-  baseStyle:{
-    width:"100%",
-    maxHeight:"40vh",
-    overflowY:"scroll",
-    flexDirection:"column",
-    alignItems:"start",
-    justifyContent:"center"
-  }
-})
+  baseStyle: {
+    width: "100%",
+    maxHeight: "30vh",
+    overflowY: "scroll",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "start",
+  },
+});
 
 /**
  * chakra ui form 사용법 자세히 알아보자
@@ -61,28 +53,28 @@ const ListContainer = chakra(Flex, {
  *  그냥 form 적용해야함!
  */
 
-const AddTodoForm = chakra('form', {
-  baseStyle:{
-    width:"100%",
-    height:"3.5rem",
-    display:"flex",
-    justifyContent:"space-between",
-    alignItems:"center",
-    padding:`0.25rem 0.5rem`
-  }
-})
+const AddTodoForm = chakra("form", {
+  baseStyle: {
+    width: "100%",
+    height: "3.5rem",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: `0.25rem 0.5rem`,
+  },
+});
 
-const AddTodoInput = chakra('input',{
-  baseStyle:{
-    width:"88%",
-    height:"80%",
-    borderRadius:"0.8rem",
-    border:"1px",
-    borderColor:"gray.400",
-    paddingX:"0.5rem",
-    textTransform:"capitalize"
-  }
-})
+const AddTodoInput = chakra("input", {
+  baseStyle: {
+    width: "88%",
+    height: "80%",
+    borderRadius: "0.8rem",
+    border: "1px",
+    borderColor: "gray.400",
+    paddingX: "0.5rem",
+    textTransform: "capitalize",
+  },
+});
 
 /**
  * 이 부분은 조금더 연구가 필요함!
@@ -90,7 +82,9 @@ const AddTodoInput = chakra('input',{
  */
 
 export default function TodoList() {
-  const [selectedStatus, setSelectedStatus] = useState<OptionsType>(Options.TO_DO)
+  const [selectedStatus, setSelectedStatus] = useState<OptionsType>(
+    Options.TO_DO
+  );
 
   const [newTodo, setNewTodo] = useState<TodoListItemType>({
     task: "",
@@ -98,55 +92,92 @@ export default function TodoList() {
     progress: Options.TO_DO,
   });
 
-  const [todos, setTodos] = useState<Array<TodoListItemType>>([])
+  const [todos, setTodos] = useState<Array<TodoListItemType>>([]);
 
+  const changeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    e.preventDefault();
+    /*스프레드 순서 기억! 뒤에 붙이면 앞의 값이 초기화됨 */
+    setNewTodo({
+      ...newTodo,
+      task: e.target.value,
+      id: crypto.randomUUID(),
+    });
+  };
 
-  const changeHandler:React.ChangeEventHandler<HTMLInputElement>=(e) => {
-      e.preventDefault()
-      /*스프레드 순서 기억! 뒤에 붙이면 앞의 값이 초기화됨 */
-        setNewTodo({
-          ...newTodo,
-          task:e.target.value,
-          id:crypto.randomUUID(),
-        })
+  const submitHandler: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    setTodos([...todos, newTodo]);
+    setNewTodo({
+      task: "",
+      id: "",
+      progress: Options.TO_DO,
+    });
+    console.log(todos);
+  };
+  /*
+  const editTodo = (id:string,newTask:string, progress:Options ) => {
+    const target = todos.filter((todo) => todo.id === id)
+
   }
+*/
 
-  const submitHandler:React.FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault()
-      setTodos([...todos,newTodo])
-      setNewTodo({
-        task:"",
-        id:"",
-        progress: Options.TO_DO,
-      })
-      console.log(todos)
-  }
+  const deleteTodo = (id: string) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
 
-  const handleProgress = (target:OptionsType) => {
-    return setSelectedStatus(target)
-  } 
+  const handleProgress = (target: OptionsType) => {
+    return setSelectedStatus(target);
+  };
 
   return (
     <TodoListLayout>
       <TodoListHeader>
         <TodoListHeading>
-          <Text color={"white"} fontSize={"2xl"} casing={"capitalize"} fontWeight={"semibold"}>
+          <Text
+            color={"white"}
+            fontSize={"2xl"}
+            casing={"capitalize"}
+            fontWeight={"semibold"}
+          >
             TodoList
           </Text>
         </TodoListHeading>
-      <ProgressSelector 
-        selectedOption={selectedStatus} 
-        handleProgress={handleProgress}/>
+        <ProgressSelector
+          selectedOption={selectedStatus}
+          handleProgress={handleProgress}
+        />
       </TodoListHeader>
       <ListContainer>
-
+        {todos &&
+          todos.map((todo: TodoListItemType) => {
+            return (
+              <TodoCard
+                deleteTodo={deleteTodo}
+                task={todo.task}
+                id={todo.id}
+                progress={todo.progress}
+              />
+            );
+          })}
       </ListContainer>
-      <AddTodoForm onSubmit={submitHandler} >
-        <AddTodoInput onChange={changeHandler} value={newTodo.task} placeholder="add new todos here" />
-        <Button width={"2rem"} height={"2rem"} justifyContent={"center"} alignItems={"center"} padding={0} type={"submit"} >
-          <AddIcon/>
+      <AddTodoForm onSubmit={submitHandler}>
+        <AddTodoInput
+          onChange={changeHandler}
+          value={newTodo.task}
+          placeholder="add new todos here"
+        />
+        <Button
+          width={"2rem"}
+          height={"2rem"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          padding={0}
+          type={"submit"}
+        >
+          <AddIcon />
         </Button>
       </AddTodoForm>
     </TodoListLayout>
-  )
+  );
 }
